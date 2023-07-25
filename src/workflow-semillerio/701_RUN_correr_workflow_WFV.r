c61c03d43393c3b36ead6ef07702846b20a070b8
@andresdla <andresimpuestos@gmail.com>
2023-07-25 19:52:27 -0300

    Create 701_RUN_correr_workflow_WFV.r

---
+++ src/workflow-semillerio/701_RUN_correr_workflow_WFV.r
@@ -0,0 +1,35 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+source("~/labo2023r/src/workflow-semillerio/711_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-semillerio/721_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-semillerio/731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy.r")
+
+#WFV_01
+source("~/cv-wfv/src/651_HT_lightgbm_WFV_01.r")
+source("~/cv-wfv/src/661_ZZ_final_WFV_01.r")
+
+#WFV_02
+
+source("~/cv-wfv/src/651_HT_lightgbm_WFV_02.r")
+source("~/cv-wfv/src/661_ZZ_final_WFV_02.r")
+
+#WFV_03
+source("~/cv-wfv/src/651_HT_lightgbm_WFV_03.r")
+source("~/cv-wfv/src/661_ZZ_final_WFV_03.r")
+
+#WFV_04
+source("~/cv-wfv/src/651_HT_lightgbm_WFV_04.r")
+source("~/cv-wfv/src/661_ZZ_final_WFV_04.r")
+
+#WFV_05
+source("~/cv-wfv/src/651_HT_lightgbm_WFV_05.r")
+source("~/cv-wfv/src/661_ZZ_final_WFV_05.r")
