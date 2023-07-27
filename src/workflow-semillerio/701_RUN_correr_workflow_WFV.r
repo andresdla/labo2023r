94bbd4abb4b7576ca8ebe68ae101a1c05cba7230
andresdla <andresimpuesto@gmail.com>
2023-07-27 22:04:00 +0000

    auto commit 1       20230727 220359

--- src/workflow-semillerio/701_RUN_correr_workflow_WFV.r
+++ src/workflow-semillerio/701_RUN_correr_workflow_WFV.r
@@ -15,25 +15,25 @@ source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy.r")
 
 #WFV_01
 source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_WFV_01.r")
-source("~/labo2023r/src/751_HT_lightgbm_WFV_01.r")
-source("~/labo2023r/src/761_ZZ_final_semillerio_WFV_01.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_WFV_01.r")
+source("~/labo2023r/src/771_ZZ_final_semillerio_WFV_01.r")
 
 #WFV_02
 source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_WFV_02.r")
-source("~/labo2023r/src/751_HT_lightgbm_WFV_02.r")
-source("~/labo2023r/src/761_ZZ_final_semillerio_WFV_02.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_WFV_02.r")
+source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio_WFV_02.r")
 
 #WFV_03
 source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_WFV_03.r")
-source("~/labo2023r/src/751_HT_lightgbm_WFV_03.r")
-source("~/labo2023r/src/761_ZZ_final_semillerio_WFV_03.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_WFV_03.r")
+source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio_WFV_03.r")
 
 #WFV_04
 source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_WFV_04.r")
-source("~/labo2023r/src/751_HT_lightgbm_WFV_04.r")
-source("~/labo2023r/src/761_ZZ_final_semillerio_WFV_04.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_WFV_04.r")
+source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio_WFV_04.r")
 
 #WFV_05
 source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_WFV_05.r")
-source("~/labo2023r/src/751_HT_lightgbm_WFV_05.r")
-source("~/labo2023r/src/761_ZZ_final_semillerio_WFV_05.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_WFV_05.r")
+source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio_WFV_05.r")
